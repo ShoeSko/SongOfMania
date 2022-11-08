e118725b60f30df1e0216120e8f12fbbdc060775
Sebastian K Øie <sebagnus@gmail.com>
2022-11-08 16:44:34 +0100

    Billboard Look at
    Created a simple billboard look at.
    Has a few options for testing/Tweaking
    16 different versions.

--- Assets/_Scripts/Movement/PlayerNavMesh.cs
+++ Assets/_Scripts/Movement/PlayerNavMesh.cs
@@ -2,27 +2,35 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
-
+//You Must have a NavMeshAgent to use this Script
+[RequireComponent(typeof(NavMeshAgent))]
 public class PlayerNavMesh : MonoBehaviour
 {
+    [Header("Billboard Customization")]
+    [SerializeField] private bool useStaticBillboard;
+    [Tooltip("0=No Restriction, 1=X only, 2=Y only, 3=Z only, 4=X&Y, 5=X&Z, 6= Y&Z, 7=XYZ")]
+    [SerializeField][Range(0,6)] private int billboardRestrictionOption;
 
+
+    //Simple Refrence to the NavMeshAgent on the object that is to move(This case player)
     private NavMeshAgent playerNavAgent;
 
+    [Header("Needed Components")]
+    //This refrences a Camera, if there is a camera shift in play, this MUST be changed as well. Make adaptable
     [SerializeField] private Camera cam;
+
     public GameObject targetDestination;
-    //[SerializeField] private Transform movePositionTransform;
 
-
+    [SerializeField] private GameObject billboardChild;
 
     private void Awake()
     {
         playerNavAgent = GetComponent<NavMeshAgent>();
+
     }
 
     private void Update()
     {
-        //Sett Destination of NavmeshAgent
-        //navMeshAgent.destination = movePositionTransform.position;
         PlayerClick();
     }
 
@@ -50,4 +58,50 @@ public class PlayerNavMesh : MonoBehaviour
             //Player is now walking
         }
     }
+
+    private void LateUpdate()
+    {
+        Billboarding();
+    }
+
+    private void Billboarding()
+    {
+        if (!useStaticBillboard)
+        {
+            /*billboardChild.*/transform.LookAt(cam.transform);
+        }
+        else
+        {
+            transform.rotation = cam.transform.rotation;
+        }
+
+        //Goes through the different versions of billboarding
+        switch (billboardRestrictionOption)
+        {
+            case 0:
+                break;
+            case 1:
+                transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles.x, 0f, 0f);
+                break;
+            case 2:
+                transform.rotation = Quaternion.Euler(0f, transform.rotation.eulerAngles.y, 0f);
+                break;
+            case 3:
+                transform.rotation = Quaternion.Euler(0f, 0f, transform.rotation.eulerAngles.z);
+                break;
+            case 4:
+                transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles.x, transform.rotation.eulerAngles.y, 0f);
+                break;
+            case 5:
+                transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles.x, 0f, transform.rotation.eulerAngles.z);
+                break;
+            case 6:
+                transform.rotation = Quaternion.Euler(0f, transform.rotation.eulerAngles.y, transform.rotation.eulerAngles.z);
+                break;
+            case 7:
+                transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles.x, transform.rotation.eulerAngles.y, transform.rotation.eulerAngles.z);
+                break;
+        }
+    }
+
 }
