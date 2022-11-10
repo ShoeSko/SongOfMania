4412c8b03c64300e1d84993826eb5743ddcda2d3
Sebastian K Øie <sebagnus@gmail.com>
2022-11-10 17:32:11 +0100

    Dialogue prevent actions
    Dialogue will freeze all other actions, unfreeze upon finish.

---
+++ Assets/_Scripts/Movement/Billboard.cs
@@ -0,0 +1,73 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Billboard : MonoBehaviour
+{
+    [Header("Billboard Customization")]
+    [Tooltip("")]
+    [SerializeField] private bool useBillboard = true;
+    [Tooltip("")]
+    [SerializeField] private bool useStaticBillboard;
+    [Tooltip("0=No Restriction, 1=X only, 2=Y only, 3=Z only, 4=X&Y, 5=X&Z, 6= Y&Z, 7=XYZ")]
+    [SerializeField] [Range(0, 6)] private int billboardRestrictionOption = 2;
+
+    [Header("Needed Components")]
+    //This refrences a Camera, if there is a camera shift in play, this MUST be changed as well. Make adaptable
+    [SerializeField] private Camera cam;
+
+
+    private void Start()
+    {
+        //Set start camera to main camera
+        cam = Camera.main;
+    }
+    private void LateUpdate()
+    {
+        if (useBillboard)
+        {
+            Billboarding();
+        }
+    }
+
+    private void Billboarding()
+    {
+        if (!useStaticBillboard)
+        {
+            /*billboardChild.*/
+            transform.LookAt(cam.transform);
+        }
+        else
+        {
+            transform.rotation = cam.transform.rotation;
+        }
+
+        //Goes through the different versions of billboarding
+        switch (billboardRestrictionOption)
+        {
+            case 0:
+                break;
+            case 1:
+                transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles.x, 0f, 0f);
+                break;
+            case 2:
+                transform.rotation = Quaternion.Euler(0f, transform.rotation.eulerAngles.y, 0f);
+                break;
+            case 3:
+                transform.rotation = Quaternion.Euler(0f, 0f, transform.rotation.eulerAngles.z);
+                break;
+            case 4:
+                transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles.x, transform.rotation.eulerAngles.y, 0f);
+                break;
+            case 5:
+                transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles.x, 0f, transform.rotation.eulerAngles.z);
+                break;
+            case 6:
+                transform.rotation = Quaternion.Euler(0f, transform.rotation.eulerAngles.y, transform.rotation.eulerAngles.z);
+                break;
+            case 7:
+                transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles.x, transform.rotation.eulerAngles.y, transform.rotation.eulerAngles.z);
+                break;
+        }
+    }
+}
--- Assets/_Scripts/Movement/PlayerNavMesh.cs
+++ Assets/_Scripts/Movement/PlayerNavMesh.cs
@@ -3,18 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 //You Must have a NavMeshAgent to use this Script
-[RequireComponent(typeof(NavMeshAgent))]
+[RequireComponent(typeof(NavMeshAgent), typeof(Billboard))]
 public class PlayerNavMesh : MonoBehaviour
 {
-    [Header("Billboard Customization")]
-    [Tooltip("")]
-    [SerializeField] private bool useBillboard;
-    [Tooltip("")]
-    [SerializeField] private bool useStaticBillboard;
-    [Tooltip("0=No Restriction, 1=X only, 2=Y only, 3=Z only, 4=X&Y, 5=X&Z, 6= Y&Z, 7=XYZ")]
-    [SerializeField][Range(0,6)] private int billboardRestrictionOption;
-
-
     //Simple Refrence to the NavMeshAgent on the object that is to move(This case player)
     private NavMeshAgent playerNavAgent;
 
@@ -61,53 +52,4 @@ public class PlayerNavMesh : MonoBehaviour
             //Player is now walking
         }
     }
-
-    private void LateUpdate()
-    {
-        if (useBillboard)
-        {
-            Billboarding();
-        }
-    }
-
-    private void Billboarding()
-    {
-        if (!useStaticBillboard)
-        {
-            /*billboardChild.*/transform.LookAt(cam.transform);
-        }
-        else
-        {
-            transform.rotation = cam.transform.rotation;
-        }
-
-        //Goes through the different versions of billboarding
-        switch (billboardRestrictionOption)
-        {
-            case 0:
-                break;
-            case 1:
-                transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles.x, 0f, 0f);
-                break;
-            case 2:
-                transform.rotation = Quaternion.Euler(0f, transform.rotation.eulerAngles.y, 0f);
-                break;
-            case 3:
-                transform.rotation = Quaternion.Euler(0f, 0f, transform.rotation.eulerAngles.z);
-                break;
-            case 4:
-                transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles.x, transform.rotation.eulerAngles.y, 0f);
-                break;
-            case 5:
-                transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles.x, 0f, transform.rotation.eulerAngles.z);
-                break;
-            case 6:
-                transform.rotation = Quaternion.Euler(0f, transform.rotation.eulerAngles.y, transform.rotation.eulerAngles.z);
-                break;
-            case 7:
-                transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles.x, transform.rotation.eulerAngles.y, transform.rotation.eulerAngles.z);
-                break;
-        }
-    }
-
 }
