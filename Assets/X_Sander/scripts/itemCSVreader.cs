69d4386aadb24cb06e5b0e2d23d0d2ca61ba0560
Sander <SaWIEn99@outlook.com>
2022-11-10 22:19:23 +0100

    CSV and TSV item and interact scripts

--- Assets/X_Sander/scripts/itemCSVreader.cs
+++ Assets/X_Sander/scripts/itemCSVreader.cs
@@ -11,12 +11,11 @@ public class itemCSVreader : MonoBehaviour
     public class Item
     {
         public string name;
-        public string PickupPrompt;
-        public string FailedPromt;
-        public string sucsessPromt;
         public string inspectPromt;
-        public bool singleUse;
+        public string FailedPromt;
         public int comboItem;
+        public int combineResult;
+        public string PickupPrompt;
     }
     [System.Serializable]
     public class ItemList
@@ -32,22 +31,22 @@ public class itemCSVreader : MonoBehaviour
 
     void readItemCSV()
     {
-        string[] data = itemDataSheet.text.Split(new string[] { ",", "\n" }, StringSplitOptions.None);
+        string[] data = itemDataSheet.text.Split(new string[] { "\t", "\n" }, StringSplitOptions.None);
 
         //Variable for later, The amount of colloms in the CSV is the first numer and the secondnumber dissreguards the firts row
-        int tableSize = data.Length / 7 - 1;
+        int tableSize = data.Length / 6 - 1;
         myItemList.item = new Item[tableSize];
 
         for (int i = 0; i < tableSize; i++)
         {
             myItemList.item[i] = new Item();
-            myItemList.item[i].name = data[7 * (i + 1)];
-            myItemList.item[i].PickupPrompt = data[7 * (i + 1) + 1];
-            myItemList.item[i].FailedPromt = data[7 * (i + 1) + 2];
-            myItemList.item[i].sucsessPromt = data[7 * (i + 1) + 3];
-            myItemList.item[i].inspectPromt = data[7 * (i + 1) + 4];
-            myItemList.item[i].singleUse = bool.Parse(data[7 * (i + 1) + 5]);
-            myItemList.item[i].comboItem = int.Parse(data[7 * (i + 1) + 6]);
+            myItemList.item[i].name = data[6 * (i + 1)];
+            myItemList.item[i].inspectPromt = data[6 * (i + 1)+1];
+            myItemList.item[i].FailedPromt = data[6 * (i + 1)+2];
+            myItemList.item[i].comboItem= int.Parse(data[6 * (i + 1)+3]);
+            myItemList.item[i].combineResult= int.Parse(data[6 * (i + 1)+4]);
+            myItemList.item[i].PickupPrompt= data[6 * (i + 1)+5];
+            
 
         }
     }
