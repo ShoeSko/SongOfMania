6a362a3a7fbd21a1926746f40b83626b66d50591
Sebastian K Øie <sebagnus@gmail.com>
2022-11-15 11:17:22 +0100

    Spillexpo edits
    Fixes of Menu and end screen + scaling of UI

--- Assets/_Scripts/Menu/basicMainMenu.cs
+++ Assets/_Scripts/Menu/basicMainMenu.cs
@@ -5,16 +5,28 @@ using UnityEngine.SceneManagement;
 
 public class basicMainMenu : MonoBehaviour
 {
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.LeftControl) && Input.GetKeyDown(KeyCode.Q))
+        {
+            mainMenu();
+        }
+    }
     public void startDemo()
     {
+        Debug.Log("loading game");
+
         SceneManager.LoadScene(1);
     }
     public void endSCreen()
     {
+        Debug.Log("loading endsceene");
         SceneManager.LoadScene(2);
     }
     public void mainMenu()
     {
+        Debug.Log("loading mainmenu");
+
         SceneManager.LoadScene(0);
     }
 }
