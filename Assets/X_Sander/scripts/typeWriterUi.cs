d5c238a250684a063dbd574205c138873bc8816a
Sander <SaWIEn99@outlook.com>
2022-11-18 12:55:55 +0100

    typewriter
    Added typewriter effect to UI

---
+++ Assets/X_Sander/scripts/typeWriterUi.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class typeWriterUi : MonoBehaviour
+{
+    public int charIndex;
+    public float typeSpeed;
+    static string text;
+    public bool isGoing;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            charIndex = text.Length;
+        }
+    }
+    public void run(string textToType, TMP_Text textLable)
+    {
+        text = textToType;
+            print("starting");
+            charIndex = 0;
+            StartCoroutine(TypeText(textToType, textLable));
+    }
+    private IEnumerator TypeText(string textToType, TMP_Text textLable)
+    {
+        isGoing = true;
+        float t = 0;
+        charIndex = 0;
+        while (charIndex < textToType.Length)
+        {
+            t += Time.deltaTime * typeSpeed;
+            charIndex = Mathf.FloorToInt(t);
+            charIndex = Mathf.Clamp(charIndex, 0, textToType.Length);
+
+            textLable.text = textToType.Substring(0, charIndex);
+
+            yield return null;
+        }
+        isGoing = false;
+    }
+}
